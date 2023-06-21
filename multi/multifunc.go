97e0b04022ddfd0ff187089129dc15b831fd29f2
Hizume Naoki <hizumee@gmail.com>
2023-06-21 16:25:37 +0900

    define multiFunc

--- multi/multifunc.go
+++ multi/multifunc.go
@@ -1,1 +1,33 @@
 package multi
+
+// OnceFunc returns a function that invokes f only once. The returned function
+// may be called concurrently.
+//
+// If f panics, the returned function will panic with the same value on every call.
+func MultiFunc(f func(), max_count uint32) func() {
+	var (
+		valid bool
+		p     any
+	)
+	multi := NewMulti(max_count)
+	// Construct the inner closure just once to reduce costs on the fast path.
+	g := func() {
+		defer func() {
+			// panicしなくてもcall ok?
+			p = recover()
+			if !valid {
+				// Re-panic immediately so on the first call the user gets a
+				// complete stack trace into f.
+				panic(p)
+			}
+		}()
+		f()
+		valid = true // Set only if f does not panic
+	}
+	return func() {
+		multi.Do(g)
+		if !valid {
+			panic(p)
+		}
+	}
+}
---
+++ multi_test/multifunc_test.go
@@ -0,0 +1,73 @@
+package multi_test
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/knowledge-work-internship-2023-6-teamA/multi-func/multi"
+)
+
+func TestMultiFunc(t *testing.T) {
+	count := 0
+	f := multi.MultiFunc(func() {
+		fmt.Println("hello world")
+		count++
+	}, 10)
+
+	for i := 0; i < 100; i++ {
+		f()
+	}
+	if count != 10 {
+		t.Errorf("count is %d, want 10", count)
+	} else {
+		t.Logf("count is %d\n", count)
+	}
+}
+
+func TestMultiFuncConcurrent(t *testing.T) {
+	var wg sync.WaitGroup
+	var count uint32
+	f := multi.MultiFunc(func() {
+		fmt.Println("hello world")
+		atomic.AddUint32(&count, 1)
+	}, 10)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go f()
+		wg.Done()
+	}
+	wg.Wait()
+	if count != 10 {
+		t.Errorf("count is %d, want 10", count)
+	} else {
+		t.Logf("count is %d\n", count)
+	}
+}
+
+// 逐次処理になっていないかテスト
+func TestMultiFuncConcurrent2(t *testing.T) {
+	var wg sync.WaitGroup
+	var count uint32
+	f := multi.MultiFunc(func() {
+		fmt.Println("start")
+		time.Sleep(1 * time.Second)
+		atomic.AddUint32(&count, 1)
+		fmt.Println("end")
+	}, 10)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go f()
+		wg.Done()
+	}
+	wg.Wait()
+	if count != 10 {
+		t.Errorf("count is %d, want 10", count)
+	} else {
+		t.Logf("count is %d\n", count)
+	}
+}
